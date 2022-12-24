38932485733dadd2d725b73e5ffc55f622ddeb14
singhabhi04 <abhishek.topa@gmail.com>
2022-12-24 17:42:38 +0530

    added/updated/deleted files

---
+++ src/main/java/com/github/pages/SearchPage.java
@@ -0,0 +1,10 @@
+package com.github.pages;
+
+public class SearchPage {
+
+	public static void main(String[] args) {
+	System.out.println("Search Page ");
+
+	}
+
+}
--- src/test/java/com/demo/tests/LoginPageTest.java
+++ src/test/java/com/demo/tests/LoginPageTest.java
@@ -4,6 +4,9 @@ public class LoginPageTest {
 
 	public static void main(String[] args) {
 		System.out.println("LoginPage Test");
+		String uname ="test";
+		String pwd ="demo@123";
+		System.out.println("Logged In sucessfully");
 
 	}
 
