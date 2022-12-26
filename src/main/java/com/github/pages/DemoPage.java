c25f99e4de44829a1d3b965d864df5eefa4b555e
singhabhi04 <abhishek.topa@gmail.com>
2022-12-26 15:34:58 +0530

    added demo page details

---
+++ src/main/java/com/github/pages/DemoPage.java
@@ -0,0 +1,9 @@
+package com.github.pages;
+
+public class DemoPage {
+
+	public static void main(String[] args) {
+		System.out.println("Demo Page");
+	}
+
+}
---
+++ src/test/java/com/demo/tests/DemoTest.java
@@ -0,0 +1,10 @@
+package com.demo.tests;
+
+public class DemoTest {
+
+	public static void main(String[] args) {
+		System.out.println("Demo Test");
+
+	}
+
+}
